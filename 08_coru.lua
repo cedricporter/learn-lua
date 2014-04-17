448ee3ee3f5310dbba48a0cdda53f46450a1057b
Cedric Porter <cedricporter@gmail.com>
2014-04-17 09:42:52 +0800

    learn coroutine

---
+++ 08_coru.lua
@@ -0,0 +1,55 @@
+#!/usr/bin/env lua
+
+co = coroutine.create(
+    function ()
+        for i = 1, 10 do
+            print("co", i)
+            coroutine.yield()
+        end
+end)
+
+coroutine.resume(co)
+coroutine.resume(co)
+coroutine.resume(co)
+coroutine.resume(co)
+
+print("---------------")
+
+function fib_gen1()
+    local fib = coroutine.create(
+        function ()
+            local a, b = 0, 1
+            while true do
+                coroutine.yield(b)
+                a, b = b, a + b
+            end
+    end)
+
+    return function ()
+        local code, res = coroutine.resume(fib)
+        return res
+    end
+end
+
+
+function fib_gen2()
+    local function fib()
+        local a, b = 0, 1
+        while true do
+            coroutine.yield(b)
+            a, b = b, a + b
+        end
+    end
+
+    return coroutine.wrap(function() fib() end)
+end
+
+for i in fib_gen1() do
+    if i > 10000 then break end
+    print(i)
+end    
+
+for i in fib_gen2() do
+    if i > 10000 then break end
+    print(i)
+end    
